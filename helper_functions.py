562e2ddecba0a793efa3a3484970f4a95b9e27f0
john8739 <jsolly@esri.com>
2020-05-11 11:51:21 -0700

    Refactoring

--- helper_functions.py
+++ helper_functions.py
@@ -1,11 +1,14 @@
-from super_item import SuperItem
+from GitHub.OrgCleanUp.super_item import SuperItem
 from arcgis import features
 
-def strip_error(error): # Error objects can have nasty carrots which mess up the html
-    return str(error).strip('<>')
+
+def strip_error(error):  # Error objects can have nasty carrots which mess up the html
+    return str(error).strip("<>")
 
 
-def get_items_from_folder(gis, folder, item_types=None) -> list: # folder=None returns the root folder
+def get_items_from_folder(
+    gis, folder, item_types=None
+) -> list:  # folder=None returns the root folder
     items = gis.users.me.items(folder=folder)
 
     if item_types:
@@ -13,10 +16,14 @@ def get_items_from_folder(gis, folder, item_types=None) -> list: # folder=None r
 
     return items
 
-def process_super_scene_service_missing_root_node(super_item): # needs work
+
+def process_super_scene_service_missing_root_node(super_item):  # needs work
     if "store" in super_item.constructed_obj.properties.keys():
-        super_item.problems.append("This scene service appears to be missing a root node. It will not be usable in ArcGIS Online or Runtime.")
+        super_item.problems.append(
+            "This scene service appears to be missing a root node. It will not be usable in ArcGIS Online or Runtime."
+        )
     return super_item
+
 
 def process_super_webmap_item(webmap_item_):
     webmap_item_ = check_super_webmap_version(webmap_item_)
@@ -29,7 +36,7 @@ def process_super_webmap_item(webmap_item_):
     if all_layers:
         for layer in all_layers:
             if "url" not in layer:
-                continue # Might want to handle these seperately. They have no url, so idk what to do. This happens with things like map notes
+                continue  # Might want to handle these seperately. They have no url, so idk what to do. This happens with things like map notes
             super_layer = SuperItem(layer)
             if super_layer.constructed_obj:
                 super_layer = process_super_layer(super_layer)
@@ -38,7 +45,7 @@ def process_super_webmap_item(webmap_item_):
 
     if webmap_item_.constructed_obj.tables:
         for table in webmap_item_.constructed_obj.tables:
-            table_obj = features.Table(url=table['url'], gis=SuperItem.GIS_OBJ)
+            table_obj = features.Table(url=table["url"], gis=SuperItem.GIS_OBJ)
             super_table = SuperItem(table_obj)
             if super_table.constructed_obj:
                 super_table = process_super_layer(super_table)
@@ -46,45 +53,55 @@ def process_super_webmap_item(webmap_item_):
             webmap_item_.layers.append(super_table)
 
     if len(webmap_item_.layers) == 0:
-        webmap_item_.problems.append(f"This webmap has no layers! What a shameful excuse of a map")
+        webmap_item_.problems.append(
+            f"This webmap has no layers! What a shameful excuse of a map"
+        )
 
     return webmap_item_
 
-def process_super_webscene_item(super_webscene_item): # needs work
+
+def process_super_webscene_item(super_webscene_item):  # needs work
     """
     Take an item, load the scene, then test URLs
     """
     scene = super_webscene_item.constructed_obj
-    all_layers = list(scene['operationalLayers'])
+    all_layers = list(scene["operationalLayers"])
 
     # #basemap layers
-    if 'baseMap' in scene:
-        if 'baseMapLayers' in scene['baseMap']:
-            all_layers += scene['baseMap']['baseMapLayers']
+    if "baseMap" in scene:
+        if "baseMapLayers" in scene["baseMap"]:
+            all_layers += scene["baseMap"]["baseMapLayers"]
 
-    #elevation/terrain
-    if 'ground' in scene:
-        if 'layers' in scene['ground']:
-            all_layers += scene['ground']['layers']
+    # elevation/terrain
+    if "ground" in scene:
+        if "layers" in scene["ground"]:
+            all_layers += scene["ground"]["layers"]
 
-    for layer in all_layers: # Multiprocessing?
+    for layer in all_layers:  # Multiprocessing?
         if "url" not in layer:
-            continue # Might want to handle these seperately. They have no url, so idk what to do. This happens with things like map notes
+            continue  # Might want to handle these seperately. They have no url, so idk what to do. This happens with things like map notes
         super_webscene_layer = SuperItem(layer)
         if super_webscene_layer.problems:
             super_webscene_item.layers.append(super_webscene_layer)
 
     return super_webscene_item
 
+
 def process_super_dashboard_item(super_dashboard_item):
     super_dashboard_item = check_super_dashboard_is_old(super_dashboard_item)
     return super_dashboard_item
+
 
 def process_super_layer(super_layer):
     super_layer_copy = super_layer
     constructed_layer = super_layer_copy.constructed_obj
 
-    super_layer_tests = [check_super_feature_layer_for_http, check_super_feature_layer_for_production_resources, check_super_feature_layer_version, check_super_feature_layer_has_no_features]
+    super_layer_tests = [
+        check_super_feature_layer_for_http,
+        check_super_feature_layer_for_production_resources,
+        check_super_feature_layer_version,
+        check_super_feature_layer_has_no_features,
+    ]
     if super_layer_copy.constructed_obj.url[-1].isdigit():
         for super_layer_test in super_layer_tests:
             super_layer_copy = super_layer_test(super_layer_copy)
@@ -99,15 +116,19 @@ def process_super_layer(super_layer):
 
     return super_layer_copy
 
+
 def check_super_dashboard_is_old(super_dashboard_item):
     dashboard_json = super_dashboard_item.constructed_obj
-    if dashboard_json['version'] < 24:
+    if dashboard_json["version"] < 24:
         super_dashboard_item.problems.append("This Dashboard version predates 1.0!")
 
-    elif dashboard_json['version'] == 40:
-        super_dashboard_item.problems.append("This Dashboard version is a middleware beta!")
+    elif dashboard_json["version"] == 40:
+        super_dashboard_item.problems.append(
+            "This Dashboard version is a middleware beta!"
+        )
 
     return super_dashboard_item
+
 
 def check_super_feature_layer_for_http(super_layer):
     if "https" not in super_layer.constructed_obj.url:
@@ -115,50 +136,63 @@ def check_super_feature_layer_for_http(super_layer):
 
     return super_layer
 
+
 def check_super_feature_layer_version(super_layer):
     constructed_layer = super_layer.constructed_obj
     arcgis_server_version = constructed_layer.properties.currentVersion
     if arcgis_server_version <= 9.3:
-        super_layer.problems.append(f"feature layer is pretty old. The ArcGIS server version is {arcgis_server_version}")
+        super_layer.problems.append(
+            f"feature layer is pretty old. The ArcGIS server version is {arcgis_server_version}"
+        )
 
     return super_layer
+
 
 def check_super_feature_layer_for_production_resources(super_layer):
     constructed_layer = super_layer.constructed_obj
 
     if super_layer.SERVICE_RE_PROD.match(constructed_layer.url):
-        super_layer.problems.append(f"This Feature Layer uses production resources {constructed_layer.url}")
+        super_layer.problems.append(
+            f"This Feature Layer uses production resources {constructed_layer.url}"
+        )
 
     return super_layer
 
 
-def check_public_with_editing_enabled(super_layer):
-    important_keys = ["allowAnonymousToDelete", "allowAnonymousToUpdate"]
-    constructed_layer = super_layer.constructed_obj
-    if hasattr(constructed_layer, "properties"): # Not sure if I need to do this
-        if "Editing" in constructed_layer.properties.capabilities:
-            super_layer.problems.append(f"This layer allows anonomous editing!!")
-    return super_layer
+# def check_public_with_editing_enabled(super_layer):
+#     important_keys = ["allowAnonymousToDelete", "allowAnonymousToUpdate"]
+#     constructed_layer = super_layer.constructed_obj
+#     if hasattr(constructed_layer, "properties"):  # Not sure if I need to do this
+#         if "Editing" in constructed_layer.properties.capabilities:
+#             super_layer.problems.append(f"This layer allows anonomous editing!!")
+#     return super_layer
 
 
 def check_super_webmap_version(webmap_item_):
     constructed_web_map = webmap_item_.constructed_obj
     webmap_spec = constructed_web_map.definition.version
     if int(webmap_spec[0]) < 2:
-        webmap_item_.problems.append(f"This webmap is outdated. It's webmap spec is {webmap_spec}")
+        webmap_item_.problems.append(
+            f"This webmap is outdated. It's webmap spec is {webmap_spec}"
+        )
 
     return webmap_item_
+
 
 def check_super_max_record_count(super_layer):
     constructed_layer = super_layer.constructed_obj
     total_features = constructed_layer.query(return_count_only=True)
 
     if "maxRecordCount" not in constructed_layer.properties:
-        super_layer.problems.append("This layer does not advertize its max record count!")
+        super_layer.problems.append(
+            "This layer does not advertize its max record count!"
+        )
         return super_layer
 
     if constructed_layer.properties.maxRecordCount < total_features:
-        super_layer.problems.append("The max record count is less than the total feature count")
+        super_layer.problems.append(
+            "The max record count is less than the total feature count"
+        )
 
     return super_layer
 
@@ -166,7 +200,10 @@ def check_super_max_record_count(super_layer):
     # feature_layer.properties.standardMaxRecordCount
     # print (feature_layer.properties.tileMaxRecordCount
 
-def check_super_feature_layer_has_no_features(super_feature_layer): # Did it this way because this is also checking to see if the layer can query
+
+def check_super_feature_layer_has_no_features(
+    super_feature_layer,
+):  # Did it this way because this is also checking to see if the layer can query
     feature_layer = super_feature_layer.constructed_obj
 
     try:
@@ -175,15 +212,16 @@ def check_super_feature_layer_has_no_features(super_feature_layer): # Did it thi
         if feature_count == 0:
             super_feature_layer.problems.append("This layer has no features!")
 
-    except RuntimeError as e: # happens when querying is not supported
+    except RuntimeError as e:  # happens when querying is not supported
         super_feature_layer.problems.append(e)
 
     except Exception as e:
-        print (e)
+        print(e)
         clean_error = strip_error(e)
         super_feature_layer.problems.append(clean_error)
 
     return super_feature_layer
+
 
 def run_tests(super_item):
     obj_test_dict = {
@@ -192,9 +230,10 @@ def run_tests(super_item):
         "FeatureLayerCollection": process_super_layer,
         "SceneLayer": process_super_scene_service_missing_root_node,
         "Webmap": process_super_webmap_item,
-        "Webscene": process_super_webscene_item
+        "Webscene": process_super_webscene_item,
     }
     return obj_test_dict[super_item.constructed_obj_type](super_item)
+
 
 if __name__ == "__main__":
     print("I'm running all by myself!")
--- org_clean_up.py
+++ org_clean_up.py
@@ -3,8 +3,8 @@ import argparse
 import asyncio
 from functools import wraps
 from jinja2 import Environment, FileSystemLoader
-from super_item import SuperItem
-import helper_functions as helper
+from GitHub.OrgCleanUp.super_item import SuperItem
+import GitHub.OrgCleanUp.helper_functions as helper
 from arcgis import gis
 
 # need to check for no features
@@ -14,15 +14,14 @@ from arcgis import gis
 #         return f(*args, **kwargs)
 #     return wrapped
 
-#@async_process_item
-def process_item(index, item) -> SuperItem:
+# @async_process_item
+def process_item(index, item):
     print(f"I've just started working on item {index} with itemid {item.itemid}")
 
     super_item = SuperItem(item)
     # a = super_item.get_constructed_obj()
-    if super_item.constructed_obj is not None: # if the object was able to be created
-        super_item = helper.run_tests(super_item) # run the tests for this 
-
+    if super_item.constructed_obj is not None:  # if the object was able to be created
+        super_item = helper.run_tests(super_item)  # run the tests for this
 
     if super_item.problems:
         return super_item
@@ -34,6 +33,7 @@ def process_item(index, item) -> SuperItem:
     else:
         return False
 
+
 # def process_org_async(items):
 #     loop = asyncio.get_event_loop()
 #     tasks = [loop.create_task(process_item(index, item)) for index, item in enumerate(items)]
@@ -44,7 +44,8 @@ def process_item(index, item) -> SuperItem:
 #     finally:
 #         loop.close()
 
-    # serialize_org_report(super_items)
+# serialize_org_report(super_items)
+
 
 def process_org_sequentially(items):
     super_items = []
@@ -52,26 +53,27 @@ def process_org_sequentially(items):
         super_item = process_item(index, item)
         if super_item:
             super_items.append(super_item)
-            
+
     serialize_org_report(super_items)
 
 
 def serialize_org_report(super_items):
-    file_loader = FileSystemLoader('templates')
+    file_loader = FileSystemLoader("templates")
     env = Environment(loader=file_loader)
-    template = env.get_template('template.txt')
+    template = env.get_template("template.txt")
     output = template.render(super_items=super_items, gis=SuperItem.GIS_OBJ)
 
     with open("org_clean_up.html", "w") as writer_obj:
         writer_obj.write(output)
 
 
-def get_items_from_folders(gis, folders:list, item_types=None) -> list: # folder=None returns the root folder
+def get_items_from_folders(
+    gis, folders: list, item_types=None
+) -> list:  # folder=None returns the root folder
     all_items = []
     for folder in folders:
         folder_items = gis.users.me.items(folder=folder)
         all_items.extend(folder_items)
-
 
     if item_types:
         filtered_items = [item for item in all_items if item.type in item_types]
@@ -79,34 +81,48 @@ def get_items_from_folders(gis, folders:list, item_types=None) -> list: # folder
 
     return all_items
 
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-target_org", dest="target_org", help="The organization to tag items in", required=True)
+    parser.add_argument(
+        "-target_org",
+        dest="target_org",
+        help="The organization to tag items in",
+        required=True,
+    )
     parser.add_argument("-username", dest="username", help="username", required=True)
     parser.add_argument("-password", dest="password", help="pass", required=True)
     args = parser.parse_args()
 
-    #os.chdir('~/Documents/org_clean_up')
+    # os.chdir('~/Documents/org_clean_up')
     SUPER_ITEM = SuperItem
-    SUPER_ITEM.GIS_OBJ = gis.GIS(url=args.target_org, username=args.username, password=args.password, verify_cert=False)
+    SUPER_ITEM.GIS_OBJ = gis.GIS(
+        url=args.target_org,
+        username=args.username,
+        password=args.password,
+        verify_cert=False,
+    )
     USERNAME = SUPER_ITEM.GIS_OBJ.users.me.username
-    #ITEMS = SUPER_ITEM.GIS_OBJ.content.search(f"type:('Feature Service' OR 'Map Service') owner:{USERNAME}", max_items=10, outside_org=False)
-    #ITEMS = SUPER_ITEM.GIS_OBJ.content.search(f"type:'Web Map' owner:{USERNAME}", max_items=20, outside_org=False)
-    ITEMS = SUPER_ITEM.GIS_OBJ.content.advanced_search(f"type:('Feature Service' OR 'Map Service') OR type:'Web Map' OR type:Dashboard owner:{USERNAME}", max_items=2_000)['results']
+    # ITEMS = SUPER_ITEM.GIS_OBJ.content.search(f"type:('Feature Service' OR 'Map Service') owner:{USERNAME}", max_items=10, outside_org=False)
+    # ITEMS = SUPER_ITEM.GIS_OBJ.content.search(f"type:'Web Map' owner:{USERNAME}", max_items=20, outside_org=False)
+    ITEMS = SUPER_ITEM.GIS_OBJ.content.advanced_search(
+        f"type:('Feature Service' OR 'Map Service') OR type:'Web Map' OR type:Dashboard owner:{USERNAME}",
+        max_items=2_000,
+    )["results"]
     print(f"I found {len(ITEMS)} items")
-    #ITEMS = [SUPER_ITEM.GIS_OBJ.content.get(itemid='')] 
-    #ITEMS = helper.get_items_from_folder(gis=SUPER_ITEM.GIS_OBJ, folder="Broken_Old_Depreciated_Data")
-    #ITEMS = SUPER_ITEM.GIS_OBJ.content.search(f"type:Dashboard owner: "USERNAME"", max_items=10_0000, outside_org=False)
+    # ITEMS = [SUPER_ITEM.GIS_OBJ.content.get(itemid='')]
+    # ITEMS = helper.get_items_from_folder(gis=SUPER_ITEM.GIS_OBJ, folder="Broken_Old_Depreciated_Data")
+    # ITEMS = SUPER_ITEM.GIS_OBJ.content.search(f"type:Dashboard owner: "USERNAME"", max_items=10_0000, outside_org=False)
 
     for ITEM in ITEMS:
         if ITEM.type not in SuperItem.known_item_types:
             print(ITEM.type)
-            
+
     FILTERED_ITEMS = [ITEM for ITEM in ITEMS if ITEM.type in SUPER_ITEM.supported_items]
-    #IGNORED_ITEM_IDS = [ITEM.id for ITEM in get_items_from_folders(gis=SUPER_ITEM.GIS_OBJ, folders=['_Trash_Can', 'Error_Route', 'Secured_Services', 'Vector_Tile_Layers', 'Basemaps'])]
-    #IGNORED_ITEM_IDS + ['99170654fe3e4bc7be95771de76b6a2a', '6f56ece8eef7473a9bbab065c14ec58b','4bb1f590f3ca48baa5ab1205ab6e629f']
-    #SUPER_FILTERED_ITEMS = [ITEM for ITEM in FILTERED_ITEMS if ITEM.id not in IGNORED_ITEM_IDS]
+    # IGNORED_ITEM_IDS = [ITEM.id for ITEM in get_items_from_folders(gis=SUPER_ITEM.GIS_OBJ, folders=['_Trash_Can', 'Error_Route', 'Secured_Services', 'Vector_Tile_Layers', 'Basemaps'])]
+    # IGNORED_ITEM_IDS + ['99170654fe3e4bc7be95771de76b6a2a', '6f56ece8eef7473a9bbab065c14ec58b','4bb1f590f3ca48baa5ab1205ab6e629f']
+    # SUPER_FILTERED_ITEMS = [ITEM for ITEM in FILTERED_ITEMS if ITEM.id not in IGNORED_ITEM_IDS]
     print(f"I'm going to work on {len(FILTERED_ITEMS)} items")
     START_TIME = time.time()
     process_org_sequentially(FILTERED_ITEMS)
